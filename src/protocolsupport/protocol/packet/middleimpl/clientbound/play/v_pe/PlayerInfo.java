1de87edb925a2ce98de76e4683d445745df1c1fb
DaMatrix <joey.rabil@gmail.com>
2017-06-15 20:06:29 +0200

    use actual PC skin

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/PlayerInfo.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/PlayerInfo.java
@@ -27,7 +27,7 @@ public class PlayerInfo extends MiddlePlayerInfo {
 					VarNumberSerializer.writeVarInt(serializer, 0); //entity id
 					StringSerializer.writeString(serializer, version, info.getName());
 					StringSerializer.writeString(serializer, version, "Standard_Steve");
-					ArraySerializer.writeByteArray(serializer, version, PESkin.STEVE);
+					ByteArraySerializer.writeByteArray(serializer, version, PESkin.fromUsername(info.username)); //we want to use the username and not the display name
 				}
 				return RecyclableSingletonList.create(serializer);
 			}
